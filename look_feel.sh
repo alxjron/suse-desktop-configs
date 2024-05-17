c09c358ffb998d5d0fcdd162899bf2c13f74a944
alxjron <119187496+alxjron@users.noreply.github.com>
2024-05-16 21:04:33 -0700

    minor mistake opps

--- look_feel.sh
+++ look_feel.sh
@@ -5,7 +5,7 @@ sudo cp -R themes /usr/share/
 sudo cp -R wallpapers /usr/share/
 
 # Theming
-xfconf-query --channel xfce4-desktop --list | grep last-image | xargs xfconf-query -c xfce4-desktop -s /usr/share/red-trees.jpg -p
+xfconf-query --channel xfce4-desktop --list | grep last-image | xargs xfconf-query -c xfce4-desktop -s /usr/share/wallpapers/red-trees.jpg -p
 
 xfconf-query -c xsettings -pn /Net/ThemeName -t "string" -s "Gruvbox-Material-Dark"
 xfconf-query -c xsettings -pn /Net/IconThemeName -t "string" -s "Obsidian-Red"
